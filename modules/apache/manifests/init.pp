6e27d0386709820abac734d7832779954499f9aa
Geert Eltink <xtreamwayz@gmail.com>
2015-03-11 13:44:07 +0100

    Fix phpmyadmin

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -9,22 +9,28 @@ class apache {
     require => Package['apache2']
   }
 
-  file { '/var/www':
-    ensure => 'link',
-    target => '/vagrant',
-    require => Package['apache2'],
-    notify => Service['apache2'],
-    replace => yes,
-    force => true
-  }
+  file {
+    '/var/www':
+      ensure => 'link',
+      target => '/vagrant',
+      require => Package['apache2'],
+      notify => Service['apache2'],
+      replace => yes,
+      force => true;
 
-  file { '/var/www/html':
-    ensure => 'absent',
-    force => true
+    '/var/www/html':
+      ensure => 'absent',
+      force => true;
+
+    '/etc/apache2/sites-enabled/000-default.conf':
+      ensure  => link,
+      target  => '/etc/apache2/sites-available/000-default.conf',
+      require => Package['apache2', 'php5-fpm', 'phpmyadmin'],
+      notify  => Service['apache2'];
   }
 
   exec { 'enable modules':
-    command     => "/usr/sbin/a2enmod rewrite proxy_fcgi",
+    command     => '/usr/sbin/a2enmod rewrite proxy_fcgi',
     require     => Package['apache2'],
     notify      => Service['apache2']
   }
