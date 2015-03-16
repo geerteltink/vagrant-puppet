8cb014cc092c236a36e5351635276d567d30767b
Geert Eltink <xtreamwayz@gmail.com>
2015-03-16 08:28:10 +0100

    Install global npm packages

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -8,4 +8,40 @@ class nodejs {
              'npm']:
     ensure => installed
   }
+
+  exec { 'npm-bower':
+    command => 'npm install bower -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
+
+  exec { 'npm-gulp':
+    command => 'npm install gulp -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
+
+  exec { 'npm-less':
+    command => 'npm install less -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
+
+  exec { 'npm-uglifycss':
+    command => 'npm install uglifycss -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
+
+  exec { 'npm-uglify-js':
+    command => 'npm install uglify-js -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
+
+  exec { 'npm-jshint':
+    command => 'npm install jshint -g',
+    path    => ['/usr/bin'],
+    require => Package['nodejs-legacy', 'npm'];
+  }
 }
