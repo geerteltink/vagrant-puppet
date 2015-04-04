4ab7400140e6d1d65080a98ba5ad13d6a43dd6c6
Geert Eltink <xtreamwayz@gmail.com>
2015-04-04 15:26:06 +0200

    Upgrade to latest version

--- modules/phpmyadmin/manifests/params.pp
+++ modules/phpmyadmin/manifests/params.pp
@@ -1,6 +1,6 @@
 class phpmyadmin::params {
 
     $install_path = '/usr/share'
-    $version      = '4_3_13'
+    $version      = '4_4_0'
     $secret       = 'ae6c337fb5dd70448af5ae6ba4249693d223a2b66350468cf35f11660b1aed58'
 }
