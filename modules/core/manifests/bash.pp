be45b0189fc83a62490e5c048e58961822d35cfa
Geert Eltink <xtreamwayz@gmail.com>
2015-05-01 11:53:44 +0200

    CD to /vagrant on login

--- modules/core/manifests/bash.pp
+++ modules/core/manifests/bash.pp
@@ -7,4 +7,11 @@ class core::bash (
         group => 'vagrant',
         mode  => '0644'
     }
+
+    file { '/home/vagrant/.bash_login':
+        content => template('core/bash_login.erb'),
+        owner => 'vagrant',
+        group => 'vagrant',
+        mode  => '0644'
+    }
 }
